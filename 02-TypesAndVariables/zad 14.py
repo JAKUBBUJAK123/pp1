e55456aaf166d94d3dfc5d3a0222e10f3273b5d9
Jakub Bujak <jakbuj3@gmail.com>
2022-10-10 14:33:50 +0200

    added

---
+++ 02-TypesAndVariables/zad 14.py
@@ -0,0 +1,4 @@
+name = "Jakub"
+age = 19
+height = 182
+print(F"My name is {name}, I have {age} years old, and my height is {height} cm.")
---
+++ 02-TypesAndVariables/zad15.py
@@ -0,0 +1,2 @@
+liczba = 5
+print("the value is {} and it's second power is {}".format(liczba,liczba**2))
